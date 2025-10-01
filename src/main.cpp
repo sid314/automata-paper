5cfbe28c45ff8e5a650af3c61e12f40e0a891f9d
sid314 <sidaa314@gmail.com>
2025-10-02 00:46:15 +0530

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,18 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+int main() {
+  auto window =
+      sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+  window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
-
-        window.clear();
-        window.display();
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
     }
+
+    window.clear();
+    window.display();
+  }
 }
