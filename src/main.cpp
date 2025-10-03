c594ff56d8d290517bf8a4b7c5bf083ce74aa5fa
PentaKS <kumudsaraswat7@gmail.com>
2025-10-03 20:30:27 +0530

    kal submission hai

--- src/main.cpp
+++ src/main.cpp
@@ -3,136 +3,312 @@
 #include <vector>
 
 std::vector<sf::RectangleShape> squares;
+std::vector<std::vector<int>> coordinates;
 std::vector<int> value;
+std::vector<int> lifeSpan;
 
-float division = 25;
-float space = 10;
-void createsq(float margin_left) {
+float division = 50;
+float space = 5;
+void createsq(float margin_left)
+{
 
-  float wWidth = 1280 - 100;
-  float wHeight = 1280 - 100;
-  sf::RectangleShape sq;
-  sq.setSize(
-      {(wWidth / division) - (space / 2), (wHeight / division) - (space / 2)});
-  sq.setFillColor(sf::Color(49, 41, 64));
-  int v = 0;
-  for (int i = 0; i < division; i++) {
-    for (int j = 0; j < division; j++) {
-      squares.push_back(sq);
-      value.push_back(v);
+    float wWidth = 1280 - 100;
+    float wHeight = 1280 - 100;
+    sf::RectangleShape sq;
+    sq.setSize({ (wWidth / division) - (space/2), (wHeight / division) - (space/2)});
+    sq.setFillColor(sf::Color(49, 41, 64));
+    int v = 0;
+    int two = 4;
+    for (int i = 0; i < division; i++)
+    {
+        for (int j = 0; j < division; j++)
+        {
+            squares.push_back(sq);
+            value.push_back(v);
+            lifeSpan.push_back(two);
+            coordinates.push_back({ i, j });
+        }
+
     }
-  }
 
-  for (int i = 0; i < division; i++) {
-    for (int j = 0; j < division; j++) {
-      squares[i * division + j].setPosition(
-          {(float)i * (wWidth / division) + space / 2 + margin_left / 2,
-           (float)j * (wHeight / division) + space / 2 + margin_left});
+    for (int i = 0; i < division; i++)
+    {
+        for (int j = 0; j < division; j++)
+        {
+            squares[i * division + j].setPosition({ (float)i * (wWidth / division) + space / 2 + margin_left/2, (float)j*(wHeight/division) + space / 2 + margin_left});
+        }
+
     }
-  }
+
 }
 
-void changePixel(int i = 0, int j = 0) {
-  if (value[i * division + j] == 0) {
+void changePixel(int i = 0, int j = 0)
+{
+    if (value[i * division + j] == 0)
+    {
+        value[i * division + j] = 1;
+        squares[i * division + j].setFillColor(sf::Color::Green);
+    }
+    else if (value[i * division + j] == 1)
+    {
+        value[i * division + j] = 0;
+        squares[i * division + j].setFillColor(sf::Color(49, 41, 64));
+    }
+
+}
+void fill(int i, int j)
+{
     value[i * division + j] = 1;
     squares[i * division + j].setFillColor(sf::Color::Green);
-  } else if (value[i * division + j] == 1) {
+}
+
+void kill(int i, int j)
+{
     value[i * division + j] = 0;
     squares[i * division + j].setFillColor(sf::Color(49, 41, 64));
-  }
-}
-void fill(int i, int j) {
-  value[i * division + j] = 1;
-  squares[i * division + j].setFillColor(sf::Color::Green);
 }
 
-void rule1() {
-
-  std::vector<std::vector<int>> changers;
-  for (int i = 1; i < division - 1; i++) {
-    for (int j = 1; j < division - 1; j++) {
-      if (value[i * division + j] == 1 && i != 0 && j != 0) {
-        changers.push_back({i, j});
-      }
+void rule1()
+{
+    std::vector<std::vector<int>> changers;
+    for (int i = 1; i < division - 1; i++)
+    {
+        for (int j = 1; j < division - 1; j++ )
+        {
+            if (value[i * division + j] == 1 && i != 0 && j != 0)
+            {
+                changers.push_back({i , j});
+                lifeSpan[i * division + j]--;
+            }
+            if (lifeSpan[i * division + j] <= 0)
+            {
+                //changePixel(i, j);
+                kill(i, j);
+            }
+        }
     }
-  }
-  for (auto &c : changers) {
-    changePixel(c[0], c[1]);
-    fill(c[0] - 1, c[1]);
-    fill(c[0] + 1, c[1]);
-    fill(c[0], c[1] - 1);
-    fill(c[0], c[1] + 1);
-  }
+    for (auto& c : changers)
+    {
+        changePixel(c[0], c[1]);
+        fill(c[0] - 1, c[1]);
+        fill(c[0] + 1, c[1]);
+        fill(c[0], c[1] - 1);
+        fill(c[0], c[1] + 1);
+        
+    }
+}
+int makeNBD(int in, int jn)
+{
+    std::vector<int> coll;
+    for (int i = in - 1; i < in + 2; i++)
+    {
+        for (int j = jn - 1; j < jn + 2; j++)
+        {
+            if (value[i * division + j] == 1 && i != 0 && j != 0 && i!= division && j!= division)
+            {
+                if (!(i == in && j == jn))
+                {
+                    coll.push_back((int)1);
+                }
+            }
+        }
+    }
+    return coll.size();
+
+
 }
 
-int main() {
-  const int wWidth = 1280;
-  const int wHeight = 1280;
+void rule2()
+{
+    std::vector<std::vector<int>> changers;
+    for (int i = 1; i < division-1; i++)
+    {
+        for (int j = 1; j < division-1 ; j++)
+        {
+            //fill_c(i, j);
 
-  sf::RenderWindow window(sf::VideoMode({wWidth, wHeight}), "SFML works!");
-  window.setFramerateLimit(60);
+            if (value[i * division + j] == 1 && i != 0 && j != 0)
+            {
+                changers.push_back({ i, j });
+                lifeSpan[i * division + j]--;
+                if (lifeSpan[i * division + j] <= 0)
+                {
+                    //changePixel(i, j);
+                    //kill(i, j);
+                }
+            }
 
-  sf::RectangleShape menu({wWidth, 100});
-  menu.setFillColor(sf::Color(36, 36, 46));
+            if (makeNBD(i, j) < 2 || makeNBD(i, j) > 3)
+            {
+                for (int k = 0; k < changers.size(); k++)
+                {
+                    if (i != changers[k][0] && j != changers[k][1])
+                    {
+                        kill(i, j);
 
-  sf::RectangleShape pause({170, 60});
-  menu.setFillColor(sf::Color(36, 36, 46));
-  pause.setFillColor(sf::Color(49, 41, 64));
-  pause.setPosition({1000, 30});
+                    }
+                }
+            }
+            if (value[i * division + j] == 0 && makeNBD(i, j) == 3 && i != 0 && j != 0)
+            {
+                for (int k = 0; k < changers.size(); k++)
+                {
+                    //if (i != changers[k][0] || j != changers[k][1]);
+                    {
+                        //fill(i, j);
+                    }
 
-  sf::Font font("src/fonts/NataSans-VariableFont_wght.ttf");
-  sf::Text generations(font);
-  generations.setPosition({20, 50 - 15});
-  generations.setString("Generations");
-  generations.setCharacterSize(30);
-  generations.setFillColor(sf::Color::White);
+                }
+                fill(i, j);
+            }
+                //if ()
+                //{
+                //    for (int k = 0; k < changers.size(); k++)
+                //    {
+                //        if (i != changers[k][0] && j != changers[k][1])
+                //        {
+                //            kill(i, j);
 
-  sf::Text pauset(font);
-  pauset.setPosition({1020, 50 - 15});
-  pauset.setString("pause 5s");
-  pauset.setCharacterSize(30);
-  pauset.setFillColor(sf::Color::White);
+                //        }
+                //    }
+                //}
 
-  int gen = 0;
 
-  sf::Text gen_number(font);
-  gen_number.setPosition({300, 50 - 15});
-  gen_number.setCharacterSize(30);
-  gen_number.setFillColor(sf::Color::White);
 
-  createsq(100);
-  changePixel(12, 12);
+                //std::cout << i << " , " << j << " # " << makeNBD(i, j) << std::endl;
+                //sf::sleep(sf::milliseconds(80));
+                //std::cout << firstNeightbours[0][0];
+            
 
-  while (window.isOpen()) {
-    sf::sleep(sf::milliseconds(1000));
+            //std::cout << makeNBD(i, j);
 
-    while (const std::optional event = window.pollEvent()) {
-      if (event->is<sf::Event::Closed>())
-        window.close();
-      else if (const auto *Keypressed = event->getIf<sf::Event::KeyPressed>()) {
-        if (Keypressed->scancode == sf::Keyboard::Scancode::Escape)
-          window.close();
-      } else if (const auto *LmouseClick =
-                     event->getIf<sf::Event::MouseButtonPressed>()) {
-        if (LmouseClick->button == sf::Mouse::Button::Left)
-          sf::sleep(sf::seconds(5));
-      }
+            //kill(i,j)
+
+        }
     }
+    for (int i = 1; i < division-1; i++)
+    {
+        for (int j = 1; j < division-1; j++)
+        {
 
-    window.clear(sf::Color(72, 72, 79));
-    gen++;
-    gen_number.setString(std::to_string(gen));
-    window.draw(menu);
-    window.draw(pause);
-    window.draw(pauset);
-    window.draw(generations);
-    window.draw(gen_number);
-    for (int i = 0; i < division; i++) {
-      for (int j = 0; j < division; j++) {
-        window.draw(squares[i * division + j]);
-      }
+
+        }
     }
-    rule1();
-    window.display();
-  }
+ 
+}
+
+
+int main()
+{
+    const int wWidth = 1280;
+    const int wHeight = 1280;
+
+    sf::RenderWindow window(sf::VideoMode({ wWidth, wHeight }), "SFML works!");
+    window.setFramerateLimit(60);
+
+    sf::RectangleShape menu({ wWidth, 100 });
+    menu.setFillColor(sf::Color(36, 36, 46));
+
+    sf::RectangleShape pause({ 170, 60 });
+    menu.setFillColor(sf::Color(36, 36, 46));
+    pause.setFillColor(sf::Color(49, 41, 64));
+    pause.setPosition({ 1000, 30 });
+
+    sf::Font font("src/fonts/NataSans-VariableFont_wght.ttf");
+    sf::Text generations(font);
+    generations.setPosition({ 20, 50 - 15 });
+    generations.setString("Generations");
+    generations.setCharacterSize(30);
+    generations.setFillColor(sf::Color::White);
+
+    sf::Text pauset(font);
+    pauset.setPosition({ 1020, 50 - 15 });
+    pauset.setString("pause 5s");
+    pauset.setCharacterSize(30);
+    pauset.setFillColor(sf::Color::White);
+
+    int gen = 0;
+    /////////////
+    //gen changing code here//
+    /////////////
+
+    sf::Text gen_number(font);
+    gen_number.setPosition({ 300, 50 - 15 });
+    gen_number.setCharacterSize(30);
+    gen_number.setFillColor(sf::Color::White);
+
+    createsq(100);
+    //for (int i = 0; i < 8; i++)
+    //{
+    //    for (int j = 0; j < 8; j++)
+    //    {
+    //        changePixel(i + 2, j + 1);
+    //    }
+    //}
+    changePixel(12, 12);
+    changePixel(13, 12);
+    changePixel(11, 12);
+
+    std::cout  << makeNBD(12, 11) ;
+    //changePixel(12, 12);
+
+    //sf::sleep(sf::seconds(4));
+
+    while (window.isOpen())
+    {
+        sf::sleep(sf::milliseconds(10));
+
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+            else if (const auto* Keypressed = event->getIf<sf::Event::KeyPressed>())
+            {
+                if (Keypressed->scancode == sf::Keyboard::Scancode::Escape)
+                    window.close();
+            }
+            else if (const auto* LmouseClick = event->getIf<sf::Event::MouseButtonPressed>())
+            {
+                if (LmouseClick->button == sf::Mouse::Button::Left)
+                    sf::sleep(sf::seconds(5));
+            }
+        }
+
+        //createsq(100);
+        //changePixel(0, 3);
+        /*for (int i = 0; i < 8; i++)
+        {
+            changePixel(i, i);
+            changePixel(0, i);
+        }*/
+        //changePixel(5, 5);
+        //changePixel(5, 5);
+
+
+
+
+        //sf::sleep(sf::seconds(1));
+        //std::cout << value[0 * division + 3];
+        window.clear(sf::Color(72, 72, 79));
+        gen++;
+        gen_number.setString(std::to_string(gen));
+        window.draw(menu);
+        window.draw(pause);
+        window.draw(pauset);
+        window.draw(generations);
+        window.draw(gen_number);
+        for (int i = 0; i < division; i++)
+        {
+            for (int j = 0; j < division; j++)
+            {
+                window.draw(squares[i * division + j]);
+            }
+        }
+        //rule1();
+        rule2();
+        window.display();
+        //sf::sleep(sf::seconds(2));
+        /////display//
+
+    }
 }
