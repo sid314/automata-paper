782876a35c8d0c8fdfe783b9b0ddaddc31d82278
PentaKS <kumudsaraswat7@gmail.com>
2025-10-09 20:05:25 +0530

    good parrern i discovered

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <conio.h>
 
 std::vector<sf::RectangleShape> squares;
 std::vector<std::vector<int>> coordinates;
@@ -18,21 +19,22 @@ public:
     // 2 = bring to life
 };
 int maxlives = 15;
-int lives[50][50];
+int lives[32][32];
 
 void initLives()
 {
-    for (int i = 0; i < 50; i++)
+    for (int i = 0; i < 32; i++)
     {
-        for (int j = 0; j < 50; j++)
+        for (int j = 0; j < 32; j++)
         {
             lives[i][j] = 0;
         }
     }
 }
 
-float division = 50;
-float space = 5;
+float division = 32;
+float space = 2;
+bool classicUI = false;
 void createsq(float margin_left)
 {
 
@@ -40,8 +42,17 @@ void createsq(float margin_left)
     float wHeight = 1280 - 100;
     sf::RectangleShape sq;
     sq.setSize(
-        { (wWidth / division) - (space / 2), (wHeight / division) - (space / 2) });
-    sq.setFillColor(sf::Color(49, 41, 64));
+        {(wWidth / division) - (space / 2), (wHeight / division) - (space / 2)});
+
+    if (classicUI == true)
+    {
+        sq.setFillColor(sf::Color(49, 41, 64));
+    }
+    else
+    {
+        sq.setFillColor(sf::Color::White);
+    }
+    // sq.setFillColor(sf::Color(49, 41, 64));
     int v = 0;
     int two = 4;
     for (int i = 0; i < division; i++)
@@ -51,7 +62,7 @@ void createsq(float margin_left)
             squares.push_back(sq);
             value.push_back(v);
             lifeSpan.push_back(two);
-            coordinates.push_back({ i, j });
+            coordinates.push_back({i, j});
         }
     }
 
@@ -60,8 +71,8 @@ void createsq(float margin_left)
         for (int j = 0; j < division; j++)
         {
             squares[i * division + j].setPosition(
-                { (float)i * (wWidth / division) + space / 2 + margin_left / 2,
-                 (float)j * (wHeight / division) + space / 2 + margin_left });
+                {(float)i * (wWidth / division) + space / 2 + margin_left / 2,
+                 (float)j * (wHeight / division) + space / 2 + margin_left});
         }
     }
 }
@@ -71,26 +82,56 @@ void changePixel(int i = 0, int j = 0)
     if (value[i * division + j] == 0)
     {
         value[i * division + j] = 1;
-        squares[i * division + j].setFillColor(sf::Color::Green);
+        if (classicUI == true)
+        {
+            squares[i * division + j].setFillColor(sf::Color::Green);
+        }
+        else
+        {
+            squares[i * division + j].setFillColor(sf::Color::White);
+        }
+
         lives[i][j] = maxlives;
     }
     else if (value[i * division + j] == 1)
     {
         value[i * division + j] = 0;
-        squares[i * division + j].setFillColor(sf::Color(49, 41, 64));
+        if (classicUI == true)
+        {
+            squares[i * division + j].setFillColor(sf::Color(49, 41, 64));
+        }
+        else
+        {
+            squares[i * division + j].setFillColor(sf::Color::White);
+        }
     }
 }
 void fill(int i, int j)
 {
     value[i * division + j] = 1;
-    squares[i * division + j].setFillColor(sf::Color::Green);
+    if (classicUI == true)
+    {
+        squares[i * division + j].setFillColor(sf::Color::Green);
+    }
+    else
+    {
+        squares[i * division + j].setFillColor(sf::Color::Black);
+    }
     lives[i][j] = maxlives;
 }
 
 void kill(int i, int j)
 {
     value[i * division + j] = 0;
-    squares[i * division + j].setFillColor(sf::Color(49, 41, 64));
+    if (classicUI == true)
+    {
+        squares[i * division + j].setFillColor(sf::Color(49, 41, 64));
+    }
+    else
+    {
+        squares[i * division + j].setFillColor(sf::Color::White);
+    }
+
     lives[i][j] = 0;
 }
 int makeNBD(int in, int jn)
@@ -117,11 +158,11 @@ void rule2()
 {
 
     // sf::sleep(sf::milliseconds(500));
-    Cell cells[50][50];
+    Cell cells[32][32];
 
-    for (int i = 0; i < 50; i++)
+    for (int i = 0; i < 32; i++)
     {
-        for (int j = 0; j < 50; j++)
+        for (int j = 0; j < 32; j++)
         {
             cells[i][j].state = 0;
         }
@@ -133,43 +174,54 @@ void rule2()
             int life = lives[i][j];
             int state = cells[i][j].state;
 
-      int aliveNeighbours = makeNBD(i, j);
-      if (aliveNeighbours < 2 || aliveNeighbours > 4)
-      {
-        state = 1;
-      }
-      // if (life < 1) {
-      //   state = 1;
-      // }
-      // if (aliveNeighbours == 2 || aliveNeighbours == 4) {
-      //   life -= 2;
-      // }
-      // if (aliveNeighbours == 1 || aliveNeighbours == 5) {
-      //   life -= 3;
-      // }
-      // if (aliveNeighbours == 0 || aliveNeighbours == 6) {
-      //   life -= 4;
-      // }
-      // if (aliveNeighbours == 7) {
-      //   life -= 5;
-      // }
-      // if (aliveNeighbours == 8) {
-      //   life -= 6;
-      // }
-      if (aliveNeighbours == 3) {
-        state = 2;
-        life--;
-      }
-      cells[i][j].state = state;
-      lives[i][j] = life;
+            int aliveNeighbours = makeNBD(i, j);
+              if (aliveNeighbours < 2 || aliveNeighbours > 3)
+              {
+                state = 1;
+              }
+            // if (life < 1)
+            // {
+            //     state = 1;
+            // }
+            // if (aliveNeighbours == 2 || aliveNeighbours == 4)
+            // {
+            //     life -= 2;
+            // }
+            // if (aliveNeighbours == 1 || aliveNeighbours == 5)
+            // {
+            //     life -= 3;
+            // }
+            // if (aliveNeighbours == 0 || aliveNeighbours == 6)
+            // {
+            //     life -= 4;
+            // }
+            // if (aliveNeighbours == 7)
+            // {
+            //     life -= 5;
+            // }
+            // if (aliveNeighbours == 8)
+            // {
+            //     life -= 6;
+            // }
+            if (aliveNeighbours == 3)
+            {
+                state = 2;
+                // life--;
+            }
+            cells[i][j].state = state;
+            // lives[i][j] = life;
+        }
     }
-  }
-  for (int i = 1; i < division - 1; i++) {
-    for (int j = 1; j < division - 1; j++) {
-      if (cells[i][j].state == 1) {
-        kill(i, j);
-
-      } else if (cells[i][j].state == 2) {
+    for (int i = 1; i < division - 1; i++)
+    {
+        for (int j = 1; j < division - 1; j++)
+        {
+            if (cells[i][j].state == 1)
+            {
+                kill(i, j);
+            }
+            else if (cells[i][j].state == 2)
+            {
 
                 fill(i, j);
             }
@@ -187,7 +239,6 @@ void pattern(std::string filename, int rows = 4)
         seed.push_back(temp);
     }
 
-
     int row = 0;
     int y = 0;
     for (int i = 0; i < seed.size(); i++)
@@ -202,11 +253,11 @@ void pattern(std::string filename, int rows = 4)
 
         if (seed[i] == "0")
         {
-            kill(12 + succeser, 12+y);
+            kill(10 + succeser - ((seed.size() / rows) / 2), 10 + y - rows / 2);
         }
         else if (seed[i] == "1")
         {
-            fill(12 + succeser, 12+y);
+            fill(10 + succeser - ((seed.size() / rows) / 2), 10 + y - rows / 2);
         }
         std::cout << seed[i];
     }
@@ -215,31 +266,32 @@ void pattern(std::string filename, int rows = 4)
 int main()
 {
 
+
     initLives();
     const int wWidth = 1280;
     const int wHeight = 1280;
 
-    sf::RenderWindow window(sf::VideoMode({ wWidth, wHeight }), "SFML works!");
-    window.setFramerateLimit(60);
-    window.setPosition({ 500, 0 });
+    sf::RenderWindow window(sf::VideoMode({wWidth, wHeight}), "SFML works!");
+    window.setFramerateLimit(120);
+    window.setPosition({500, 0});
 
-    sf::RectangleShape menu({ wWidth, 100 });
+    sf::RectangleShape menu({wWidth, 100});
     menu.setFillColor(sf::Color(36, 36, 46));
 
-    sf::RectangleShape pause({ 170, 60 });
+    sf::RectangleShape pause({170, 60});
     menu.setFillColor(sf::Color(36, 36, 46));
     pause.setFillColor(sf::Color(49, 41, 64));
-    pause.setPosition({ 1000, 30 });
+    pause.setPosition({1000, 30});
 
     sf::Font font("C:\\Users\\HP\\Desktop\\New folder (2)\\automata-paper\\src\\fonts\\NataSans-VariableFont_wght.ttf");
     sf::Text generations(font);
-    generations.setPosition({ 20, 50 - 15 });
+    generations.setPosition({20, 50 - 15});
     generations.setString("Generations");
     generations.setCharacterSize(30);
     generations.setFillColor(sf::Color::White);
 
     sf::Text pauset(font);
-    pauset.setPosition({ 1020, 50 - 15 });
+    pauset.setPosition({1020, 50 - 15});
     pauset.setString("pause 5s");
     pauset.setCharacterSize(30);
     pauset.setFillColor(sf::Color::White);
@@ -250,36 +302,44 @@ int main()
     /////////////
 
     sf::Text gen_number(font);
-    gen_number.setPosition({ 300, 50 - 15 });
+    gen_number.setPosition({300, 50 - 15});
     gen_number.setCharacterSize(30);
     gen_number.setFillColor(sf::Color::White);
 
     createsq(100);
 
-    pattern("C:\\Users\\HP\\Desktop\\New folder (2)\\automata-paper\\src\\try.txt", 3);
+    pattern("C:\\Users\\HP\\Desktop\\New folder (2)\\automata-paper\\src\\try.txt", 16);
 
-    std::cout << makeNBD(12, 11);
+    // std::cout << makeNBD(12, 11);
     while (window.isOpen())
     {
-        // sf::sleep(sf::milliseconds(1));
+        sf::sleep(sf::milliseconds(200));
 
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
-            else if (const auto* Keypressed = event->getIf<sf::Event::KeyPressed>())
+            else if (const auto *Keypressed = event->getIf<sf::Event::KeyPressed>())
             {
                 if (Keypressed->scancode == sf::Keyboard::Scancode::Escape)
                     window.close();
             }
-            else if (const auto* LmouseClick =
-                event->getIf<sf::Event::MouseButtonPressed>())
+            else if (const auto *LmouseClick =
+                         event->getIf<sf::Event::MouseButtonPressed>())
             {
                 if (LmouseClick->button == sf::Mouse::Button::Left)
                     sf::sleep(sf::seconds(5));
             }
         }
-        window.clear(sf::Color(72, 72, 79));
+        if (classicUI == true)
+        {
+            window.clear(sf::Color(72, 72, 79));
+        }
+        else
+        {
+            window.clear(sf::Color::White);
+        }
+
         gen++;
         gen_number.setString(std::to_string(gen));
         window.draw(menu);
@@ -298,7 +358,7 @@ int main()
         window.display();
          if (gen == 1 /*|| gen == 10 || gen == 42*/)
          {
-          sf::sleep(sf::seconds(14));
+          sf::sleep(sf::seconds(8));
          }
     }
 }
